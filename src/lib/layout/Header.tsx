49380d9609f7b552a4da0afd6d684f7c02c8a55a
geoday03 <geoday.ai.si@gmail.com>
2022-12-04 15:23:38 -0800

    a

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
           "background.dark"
         )}
         maxW="100%"
-        padding="1rem"
+        padding="10px"
         gap="1rem"
       >
         <Center w="100%">
