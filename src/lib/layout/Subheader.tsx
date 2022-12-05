be93a25954d51c8fd56d78ee75090cef4e42527c
geoday03 <geoday.ai.si@gmail.com>
2022-12-04 16:31:42 -0800

    a

--- src/lib/layout/Subheader.tsx
+++ src/lib/layout/Subheader.tsx
@@ -6,8 +6,6 @@ import {
   Spacer,
   Text,
   useColorModeValue,
-  Wrap,
-  WrapItem,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 
@@ -15,88 +13,67 @@ export default function Subheader() {
   const borderColor = useColorModeValue("text.light", "border.dark");
 
   return (
-    <Wrap
+    <Flex
       backgroundColor={useColorModeValue("background.light", "background.dark")}
       gap="7%"
-      px="20px"
       fontFamily="body"
       fontWeight="500"
-      fontSize="15px"
+      fontSize={["13px", "13px", "15px"]}
       color={useColorModeValue("text.light", "text.dark")}
       lineHeight="1"
+      overflow="auto"
+      m="auto"
+      letterSpacing="wide"
     >
       <Spacer />
 
-      <WrapItem>
-        <NextLink href="/" passHref>
-          <Link>
-            <Text borderBottomWidth="3px" borderColor={borderColor} pb="20px">
-              Home
-            </Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
+      <Center></Center>
+
+      <NextLink href="/" passHref>
+        <Link>
+          <Text borderBottomWidth="3px" borderColor={borderColor} pb="20px">
+            Home
+          </Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/" passHref>
+        <Link>
+          <Text pb="15px">Shop</Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/store/categories/clothing/men" passHref>
+        <Link>
+          <Text pb="15px">Men's Clothing</Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/store/categories/clothing/women" passHref>
+        <Link>
+          <Text pb="15px">Women's Clothing</Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/store/categories/jewelery" passHref>
+        <Link>
+          <Text pb="15px">Jewelery</Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/store/categories/electronics" passHref>
+        <Link>
+          <Text pb="15px">Electronics</Text>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/categories/among-us" passHref>
+        <Link>
+          <Text pb="15px">Among Us</Text>
+        </Link>
+      </NextLink>
 
       <Spacer />
-
-      <WrapItem>
-        <NextLink href="/" passHref>
-          <Link>
-            <Text pb="15px">Shop</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-
-      <Spacer />
-
-      <WrapItem>
-        <NextLink href="/store/categories/clothing/men" passHref>
-          <Link>
-            <Text pb="15px">Men's Clothing</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-
-      <Spacer />
-
-      <WrapItem>
-        <NextLink href="/store/categories/clothing/women" passHref>
-          <Link>
-            <Text pb="15px">Women's Clothing</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-
-      <Spacer />
-
-      <WrapItem>
-        <NextLink href="/store/categories/jewelery" passHref>
-          <Link>
-            <Text pb="15px">Jewelery</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-
-      <Spacer />
-
-      <WrapItem>
-        <NextLink href="/store/categories/electronics" passHref>
-          <Link>
-            <Text pb="15px">Electronics</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-
-      <Spacer />
-
-      <WrapItem>
-        <NextLink href="/categories/among-us" passHref>
-          <Link>
-            <Text pb="15px">Among Us</Text>
-          </Link>
-        </NextLink>
-      </WrapItem>
-      <Spacer />
-    </Wrap>
+    </Flex>
   );
 }
