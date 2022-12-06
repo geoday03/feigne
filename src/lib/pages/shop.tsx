f5164e4cf841f1b839a8b475a990014bd5582d74
geoday03 <geoday.ai.si@gmail.com>
2022-12-06 08:22:04 -0800

    a

--- src/lib/pages/shop.tsx
+++ src/lib/pages/shop.tsx
@@ -17,24 +17,7 @@ const Shop = () => {
       w="90%"
       mx="auto"
     >
-      <Image src="/feigne-member-exclusive.png" />
-      <Box pb="10px" mt="-20%">
-        <ProductRow rowTitle="New Arrivals" amount={5} />
-      </Box>
-
-      <Box pb="10px">
-        <ProductRow rowTitle="Shop Men's Clothing" amount={5} />
-      </Box>
-
-      <Box pb="10px">
-        <ProductRow rowTitle="Shop Women's Clothing" amount={5} />
-      </Box>
-
-      <ProductRow amount={5} rowTitle="Jewelry" category="jewelry" />
-
-      <Box>
-        <ProductRow amount={5} rowTitle="Clothing" category="clothing" />
-      </Box>
+      soon to be
     </Center>
   );
 };
