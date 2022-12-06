f6b81f776dac79439aebb89aeda3b88c9387c561
geoday03 <geoday.ai.si@gmail.com>
2022-12-06 01:24:58 -0800

    landing page whatever

--- src/lib/components/product/ProductRow.tsx
+++ src/lib/components/product/ProductRow.tsx
@@ -17,39 +17,43 @@ export default function ProductRow(props: {
 }) {
   return (
     <Box>
-      <Box display={{ base: "none", sm: "unset" }}>
-        <Text fontWeight="500" fontSize="20px" mb="10px">
-          {props.rowTitle}
-        </Text>
+      <Text fontWeight="500" fontSize="20px" mb="10px">
+        {props.rowTitle}
+      </Text>
 
-        <Grid
-          templateColumns={{
-            sm: "repeat(2, minmax(0,1fr))",
-            md: "repeat(3, 1fr)",
-            lg: "repeat(4, 1fr)",
-            xl: "repeat(5, 1fr)",
-          }}
-          gap={6}
-        >
-          <ProductCard key="0" />
+      <Grid
+        templateColumns={{
+          sm: "repeat(2, minmax(0,1fr))",
+          md: "repeat(3, 1fr)",
+          lg: "repeat(4, 1fr)",
+          xl: "repeat(5, 1fr)",
+        }}
+        display={{ base: "none", sm: "unset" }}
+        gap={6}
+      >
+        <ProductCard key="0" />
 
-          <Box display={{ base: "flex", md: "flex", lg: "flex" }}>
-            <ProductCard key="2" />
-          </Box>
+        <Box display={{ base: "flex", md: "flex", lg: "flex" }}>
+          <ProductCard key="2" />
+        </Box>
 
-          <Box display={{ base: "none", md: "flex", lg: "flex" }}>
-            <ProductCard key="2" />
-          </Box>
+        <Box display={{ base: "none", md: "flex", lg: "flex" }}>
+          <ProductCard key="2" />
+        </Box>
 
-          <Box display={{ base: "none", md: "none", lg: "flex" }}>
-            <ProductCard key="3" />
-          </Box>
+        <Box display={{ base: "none", md: "none", lg: "flex" }}>
+          <ProductCard key="3" />
+        </Box>
 
-          <Box display={{ base: "none", md: "none", lg: "none", xl: "flex" }}>
-            <ProductCard key="3" />
-          </Box>
-        </Grid>
-      </Box>
+        <Box display={{ base: "none", md: "none", lg: "none", xl: "flex" }}>
+          <ProductCard key="3" />
+        </Box>
+      </Grid>
+
+      <Flex gap="1rem" p="5px">
+        <ProductCard />
+        <ProductCard />
+      </Flex>
     </Box>
   );
 }
