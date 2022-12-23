40bbfbc5a8156fd9d6a1de19d3cbf886b03538be
geoday03 <geoday.ai.si@gmail.com>
2022-12-22 23:43:47 -0800

    isr implementation into product pages

--- src/lib/features/product-service.ts
+++ src/lib/features/product-service.ts
@@ -46,4 +46,29 @@ const getProducts = (config: GetProductsConfig): Product[] => {
   return productList;
 };
 
-export const productService = { getProduct, getProducts };
+const getPIDs = () => {
+  let PIDs: string[] = [];
+
+  type categoryKey = keyof typeof products;
+  type subCategoryKey = keyof typeof products[categoryKey];
+
+  Object.keys(products).map((category) => {
+    Object.keys(products[category as categoryKey]).map((subcategory) => {
+      Object.keys(
+        products[category as subCategoryKey][subcategory as subCategoryKey]
+      ).map((key) => {
+        PIDs.push(
+          (
+            products[category as categoryKey][subcategory as subCategoryKey][
+              key
+            ] as Product
+          ).id
+        );
+      });
+    });
+  });
+
+  return PIDs;
+};
+
+export const productService = { getProduct, getProducts, getPIDs };
--- src/pages/product/[pid].ts
+++ src/pages/product/[pid].ts
@@ -1,3 +1,19 @@
+import { productService } from "lib/features/product-service";
 import Product from "lib/pages/product";
+
+export async function getStaticPaths() {
+  const pids = productService.getPIDs();
+
+  const paths = pids.map((pid) => ({ params: { pid: pid } }));
+
+  return { paths, fallback: "blocking" };
+}
+
+export async function getStaticProps(params: { pid: string }) {
+  return {
+    props: {},
+    revalidate: 1,
+  };
+}
 
 export default Product;
